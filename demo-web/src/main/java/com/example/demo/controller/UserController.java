f0bca1c03ff2c993141852359a6d0db9f24cedbe
cike001 <mini_cike@163.com>
2019-04-06 22:54:36 +0800

    commit UserController

--- demo-web/src/main/java/com/example/demo/controller/UserController.java
+++ demo-web/src/main/java/com/example/demo/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
@@ -22,6 +24,12 @@ public class UserController {
 	public List<User> findAllUser(){
 		//return userService.findAllUser();
 		return userService.findAllUser();
+	}
+	
+	@RequestMapping(value="/findUserById/{id}",method=RequestMethod.GET)
+	public User findUserById(@PathVariable Integer id){
+		System.out.println(id);
+		return new User();
 	}
 	
 }
